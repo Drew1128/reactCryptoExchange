57a148df38b7bc195a4fa1b9038640737fc7af76
Drew Morrison <drewm1128@gmail.com>
2021-11-11 06:27:38 -0600

    started builing out the contract and rean more test against it

--- blockchain-developer-bootcamp/src/contracts/Token.sol
+++ blockchain-developer-bootcamp/src/contracts/Token.sol
@@ -1,5 +1,12 @@
 pragma solidity ^0.5.0;
 
 contract Token {
-    string public name = "My Name";
+    string public name = "Drew Token";
+    string public symbol = "Drew";
+    uint256 public decimals = 18;
+    uint256 public totalSupply;
+
+    constructor() public {
+        totalSupply = 1000000 * (10 ** decimals);
+    }
 }
--- blockchain-developer-bootcamp/test/Token.test.js
+++ blockchain-developer-bootcamp/test/Token.test.js
@@ -1,17 +1,39 @@
+const Token = artifacts.require('./Token')
+
 require('chai')
  .use(require('chai-as-promised'))
  .should()
 
-
-const Token = artifacts.require('./Token')
-
 contract('Token', (accounts) => {
+    const name = 'Drew Token'
+    const symbol = 'Drew'
+    const decimals = '18'
+    const totalSupply = '1000000000000000000000000'
+    let token 
+
+    beforeEach(async () => {
+        token = await Token.new()
+    })
+
     describe('deployment', () => {
         it('tracks the name', async () => {
-            const token = await Token.new() // fetch token from the blockchain
-            const result = await token.name() // read token name here...
-            result.should.equal('My Name') // token name is 'My Name'
-            
+            const result = await token.name() 
+            result.should.equal(name) 
+        })
+
+        it('tracks the symbol', async () => {
+            const result = await token.symbol() 
+            result.should.equal(symbol) 
+        })
+
+        it('tracks the name', async () => {
+            const result = await token.decimals() 
+            result.toString().should.equal(decimals) 
+        })
+
+        it('tracks the name', async () => {
+            const result = await token.totalSupply() 
+            result.toString().should.equal(totalSupply)  
         })
     })
 })
