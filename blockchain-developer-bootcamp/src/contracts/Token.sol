b46e4c8563cc829b59461c1c049609334af52202
Drew Morrison <drewm1128@gmail.com>
2021-11-13 09:17:56 -0600

    ran test against the transfer function, imported SafeMath from openzepplin library,  refactored the total supply in my test using web#

--- blockchain-developer-bootcamp/src/contracts/Token.sol
+++ blockchain-developer-bootcamp/src/contracts/Token.sol
@@ -1,16 +1,27 @@
 pragma solidity ^0.5.0;
 
+import "openzeppelin-solidity/contracts/math/SafeMath.sol"; 
+
 contract Token {
+    using SafeMath for uint;
+
     string public name = "Drew Token";
     string public symbol = "Drew";
     uint256 public decimals = 18;
     uint256 public totalSupply;
 
-    // adding a global state variable to track balances 
+    // adding a global state variable to track balances of accounts
     mapping(address => uint256) public balanceOf;
 
     constructor() public {
         totalSupply = 1000000 * (10 ** decimals);
+        // assigned the total supply to the deployer 
         balanceOf[msg.sender] = totalSupply; 
+    }
+
+    function transfer(address _to, uint256 _value) public returns (bool success) {
+        balanceOf[msg.sender] = balanceOf[msg.sender].sub(_value);
+        balanceOf[_to] = balanceOf[_to].add(_value);
+        return true;
     }
 }
--- blockchain-developer-bootcamp/test/Token.test.js
+++ blockchain-developer-bootcamp/test/Token.test.js
@@ -4,11 +4,17 @@ require('chai')
  .use(require('chai-as-promised'))
  .should()
 
-contract('Token', ([deployer]) => {
+ const tokens = (n) => {
+     return new web3.utils.BN(
+    web3.utils.toWei(n.toString(), 'ether')
+     )
+ }
+
+contract('Token', ([deployer, receiver]) => {
     const name = 'Drew Token'
     const symbol = 'Drew'
     const decimals = '18'
-    const totalSupply = '1000000000000000000000000'
+    const totalSupply = tokens(1000000).toString()
     let token 
 
     beforeEach(async () => {
@@ -33,14 +39,37 @@ contract('Token', ([deployer]) => {
 
         it('tracks the total supply', async () => {
             const result = await token.totalSupply() 
-            result.toString().should.equal(totalSupply)  
+            result.toString().should.equal(totalSupply.toString())  
         })
 
        it('assigns the total supply to the deployer', async () => {
            const result = await token.balanceOf(deployer)
-           result.toString().should.equal(totalSupply)
+           result.toString().should.equal(totalSupply.toString())
        })
 
 
+    })
+
+    describe ('sending Tokens', ()=> {
+
+        it('transfers token balances', async () =>{
+            let balanceOf
+            // before transfer
+            balanceOf = await token.balanceOf(deployer)
+            console.log('deployer balance before transfer', balanceOf.toString())
+            balanceOf = await token.balanceOf(receiver)
+            console.log("reciever balance before transfer", balanceOf.toString())
+
+            // Transfer (The from value is the function meta date)
+            await token.transfer(receiver, tokens(100), {from: deployer})
+
+            // Balances after transfer
+            balanceOf = await token.balanceOf(deployer)
+            console.log('deployer balance after transfer', balanceOf.toString())
+            balanceOf = await token.balanceOf(receiver)
+            console.log("reciever balance after transfer", balanceOf.toString())
+
+
+        })
     })
 })
